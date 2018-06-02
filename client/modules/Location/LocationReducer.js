26782c640be28b721d3843afb6b07c13b8d5f8e6
abuuzayr <abu.uzayr@builtforfifty.com>
2018-06-03 02:21:09 +0800

    Add location model, basic files, schema, routes, controllers

---
+++ client/modules/Location/LocationReducer.js
@@ -0,0 +1,14 @@
+// Import Actions
+import {  } from './LocationActions';
+
+// Initial State
+const initialState = {};
+
+const LocationReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default LocationReducer;
